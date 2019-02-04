1fa18e07b97681ebcfb83f3ed9beb5044e9484d2
Twan Natter <twan180401@gmail.com>
2019-02-04 13:27:44 +0100

    Opdracht 1

---
+++ C kennis/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace C_kennis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaratie van de variabelen
+            string naam;
+
+            //Naam opvragen
+            Console.Write("Geef je naam op: ");
+            naam = Console.ReadLine();
+
+            //Resultaat weergeven
+            Console.WriteLine("Welkom bij SoundSharp " + naam);
+            Console.ReadLine();
+        }
+    }
+}
