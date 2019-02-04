fcd0c9e6d7eb711dd954b321e91503726b7e21ac
Twan Natter <twan180401@gmail.com>
2019-02-04 14:08:42 +0100

    Opdracht 3

--- C kennis/Program.cs
+++ C kennis/Program.cs
@@ -9,27 +9,81 @@ namespace C_kennis
         {
             //Declaratie van de variabelen
             const string strWachtwoord = "SHARPSOUND";
-            string strNaam;
-            string strInputWachtwoord;
+            string strNaam, strInputWachtwoord;
+            int intPoging = 1;
 
             //Naam opvragen
             Console.Write("Geef je naam op: ");
             strNaam = Console.ReadLine();
 
-            //Wachtwoord opvragen
-            Console.Write("Geef je wachtwoord op: ");
-            strInputWachtwoord = Console.ReadLine();
-
             //Welkomstbericht weergeven
-            switch (strInputWachtwoord)
+            do
             {
-                case (strWachtwoord):
-                    Console.WriteLine("Welkom bij SoundSharp " + strNaam);
-                    break;
-                default:
-                    Console.WriteLine("Password is onjuist.");
-                    break;
+                switch (intPoging)
+                {
+                    case 1:
+                        //Wachtwoord opvragen
+                        Console.Write("Voer het wachtwoord in: ");
+                        strInputWachtwoord = Console.ReadLine();
+
+                        if (strInputWachtwoord == strWachtwoord)
+                        {
+                            Console.WriteLine("Welkom bij SoundSharp " + strNaam);
+                            return;
+                            break;
+                        }
+                        else
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("Password is onjuist.");
+                            Console.ResetColor();
+                            intPoging++;
+                            break;
+                        }
+
+                    case 2:
+                        Console.WriteLine("Poging " + intPoging + " van 3");
+                        Console.Write("Voer het wachtwoord in: ");
+                        strInputWachtwoord = Console.ReadLine();
+                        if (strInputWachtwoord == strWachtwoord)
+                        {
+                            Console.WriteLine("Welkom bij SoundSharp " + strNaam);
+                            return;
+                            break;
+                        }
+                        else
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("Password is onjuist.");
+                            Console.ResetColor();
+                            intPoging++;
+                            break;
+                        }
+
+                    case 3:
+                        Console.WriteLine("Poging " + intPoging + " van 3");
+                        Console.WriteLine("LET OP: Laatste poging! \nVoer het wachtwoord in: ");
+                        strInputWachtwoord = Console.ReadLine();
+                        if (strInputWachtwoord == strWachtwoord)
+                        {
+                            Console.WriteLine("Correct wachtwoord");
+                            Console.WriteLine("U bent ingelogd");
+                            return;
+                            break;
+
+                        }
+                        else
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("Password is onjuist.");
+                            Console.ResetColor();
+                            break;
+                        }
+
+                }
             }
+
+            while (intPoging < 4);
         }
         static void Main(string[] args)
         {
